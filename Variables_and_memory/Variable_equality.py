3be71b43312dc6fe8930d49b739d23c74503a147
mikaelbeat <ryynanenphm@gmail.com>
2020-05-18 13:52:06 +0300

    Update

---
+++ Variables_and_memory/Variable_equality.py
@@ -0,0 +1,9 @@
+
+data1 = 100
+data2 = 100
+
+print(f"Data1 memory location {id(data1)}.")
+print(f"Data2 memory location {id(data2)}.")
+
+print(f"Is data1 located in same memory address than data2 {data1 is data2}.")
+print(f"Is data1  value in same than data2 {data1 == data2}.")
